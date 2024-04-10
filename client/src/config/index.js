868c210d7db60fe5712e48166cdd1040c9350cc1
Falnadaf <101874631+Falnadaf@users.noreply.github.com>
2024-04-10 02:01:57 +0200

    Update index.js

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,3 @@
 
-export const BACKEND_URL = "https://binariumdepotserver.onrender.com";
+export const BACKEND_URL = "https://bin2.onrender.com/";
 export const HOST_URL = "http://192.168.69.92:5173";
